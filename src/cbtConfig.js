ba0bb9c85c2d2a60f75f8867a85554d01c726597
Amit Raj <navodaya2152@gmail.com>
2026-09-01 13:46:13 +0530

    feat: add external CBT configuration

---
+++ src/cbtConfig.js
@@ -0,0 +1,5 @@
+export const cbtConfig = {
+  url: 'https://class9-bseb-cbt.netlify.app/',
+  title: 'Class 9 BSEB CBT',
+  description: 'समयबद्ध परीक्षा, स्वतः परिणाम और ईमेल submission वाला CBT।',
+};
