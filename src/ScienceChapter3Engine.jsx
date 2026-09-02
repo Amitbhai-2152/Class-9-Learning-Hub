59ecb965901bf2344f676938dc0b5e1409eac5bf
Amit Raj <navodaya2152@gmail.com>
2026-09-02 06:00:11 +0530

    Fix Science Chapter 3 layout and content outline

--- src/ScienceChapter3Engine.jsx
+++ src/ScienceChapter3Engine.jsx
@@ -1,1 +1,16 @@
-export {ScienceChapter3Engine as default,ScienceChapter3Engine} from './ScienceChapter3EngineFixed';
+import React from 'react';
+import {ScienceChapter3Engine as ScienceChapter3Core} from './ScienceChapter3EngineFixed';
+import {scienceChapter3Learning} from './scienceChapter3Learning';
+import {ChapterContents} from './ChapterContents';
+import './science-chapter-sidebar.css';
+
+export function ScienceChapter3Engine(props){
+  return <div className="science-chapter-frame">
+    <ChapterContents lessons={scienceChapter3Learning.lessons} title="इस अध्याय में क्या पढ़ेंगे?" compact={false}/>
+    <div className="science-chapter-main">
+      <ScienceChapter3Core {...props}/>
+    </div>
+  </div>;
+}
+
+export default ScienceChapter3Engine;
