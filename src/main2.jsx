7659128bef31fc9c406c0e9b215c3f96d1b382e4
Amit Raj <navodaya2152@gmail.com>
2026-09-02 18:52:26 +0530

    Wire Science mode routing fix

--- src/main2.jsx
+++ src/main2.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import { AppErrorBoundary } from './AppErrorBoundary.jsx';
+import './scienceModeRouter.js';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
