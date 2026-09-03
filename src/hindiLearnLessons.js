269594d9bf13c77eec0385b460633c9f5e89feb7
Amit Raj <navodaya2152@gmail.com>
2026-09-03 17:14:29 +0530

    Add centralized Hindi Learn lesson registry

---
+++ src/hindiLearnLessons.js
@@ -0,0 +1,20 @@
+import {hindiChapter6Lesson} from './hindiChapter6Engine';
+import {hindiChapter7Lesson} from './hindiChapter7Engine';
+import {hindiChapter8Lesson} from './hindiChapter8Engine';
+import {hindiChapter9Lesson} from './hindiChapter9Engine';
+import {hindiChapter10Lesson} from './hindiChapter10Engine';
+import {hindiChapter11Lesson} from './hindiChapter11Engine';
+import {hindiChapter12Lesson} from './hindiChapter12Engine';
+import {hindiPoetry1Lesson} from './hindiPoetry1Engine';
+import {hindiPoetry2Lesson} from './hindiPoetry2Engine';
+import {hindiPoetry3Lesson} from './hindiPoetry3Engine';
+import {hindiPoetry4Lesson} from './hindiPoetry4Engine';
+import {hindiPoetry5Lesson} from './hindiPoetry5Engine';
+import {hindiPoetry6Lesson} from './hindiPoetry6Engine';
+import {hindiPoetry7Lesson} from './hindiPoetry7Engine';
+import {hindiPoetry8Lesson} from './hindiPoetry8Engine';
+
+export const hindiLearnLessons={
+  g6:hindiChapter6Lesson,g7:hindiChapter7Lesson,g8:hindiChapter8Lesson,g9:hindiChapter9Lesson,g10:hindiChapter10Lesson,g11:hindiChapter11Lesson,g12:hindiChapter12Lesson,
+  p1:hindiPoetry1Lesson,p2:hindiPoetry2Lesson,p3:hindiPoetry3Lesson,p4:hindiPoetry4Lesson,p5:hindiPoetry5Lesson,p6:hindiPoetry6Lesson,p7:hindiPoetry7Lesson,p8:hindiPoetry8Lesson
+};
