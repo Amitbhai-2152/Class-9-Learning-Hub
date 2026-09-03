af1ef8d91ec4ad5f44d17c187736e52375213aa5
Amit Raj <navodaya2152@gmail.com>
2026-09-03 17:41:50 +0530

    Add Hindi Poetry 9-12 Learn mappings

--- src/hindiLearnLessons.js
+++ src/hindiLearnLessons.js
@@ -18,9 +18,14 @@ import {hindiPoetry5Lesson} from './hindiPoetry5Engine';
 import {hindiPoetry6Lesson} from './hindiPoetry6Engine';
 import {hindiPoetry7Lesson} from './hindiPoetry7Engine';
 import {hindiPoetry8Lesson} from './hindiPoetry8Engine';
+import {hindiPoetry9Lesson} from './hindiPoetry9Engine';
+import {hindiPoetry10Lesson} from './hindiPoetry10Engine';
+import {hindiPoetry11Lesson} from './hindiPoetry11Engine';
+import {hindiPoetry12Lesson} from './hindiPoetry12Engine';
 
 export const hindiLearnLessons={
   g1:hindiChapter1Lesson,g2:hindiChapter2Lesson,g3:hindiChapter3Lesson,g4:hindiChapter4Lesson,g5:hindiChapter5Lesson,
   g6:hindiChapter6Lesson,g7:hindiChapter7Lesson,g8:hindiChapter8Lesson,g9:hindiChapter9Lesson,g10:hindiChapter10Lesson,g11:hindiChapter11Lesson,g12:hindiChapter12Lesson,
-  p1:hindiPoetry1Lesson,p2:hindiPoetry2Lesson,p3:hindiPoetry3Lesson,p4:hindiPoetry4Lesson,p5:hindiPoetry5Lesson,p6:hindiPoetry6Lesson,p7:hindiPoetry7Lesson,p8:hindiPoetry8Lesson
+  p1:hindiPoetry1Lesson,p2:hindiPoetry2Lesson,p3:hindiPoetry3Lesson,p4:hindiPoetry4Lesson,p5:hindiPoetry5Lesson,p6:hindiPoetry6Lesson,p7:hindiPoetry7Lesson,p8:hindiPoetry8Lesson,
+  p9:hindiPoetry9Lesson,p10:hindiPoetry10Lesson,p11:hindiPoetry11Lesson,p12:hindiPoetry12Lesson
 };
