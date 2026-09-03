711607da11ed947baea224669080a4ed8c1757ae
Amit Raj <navodaya2152@gmail.com>
2026-09-03 17:30:24 +0530

    Add legacy Hindi chapter Learn data mapping

--- src/hindiLearnLessons.js
+++ src/hindiLearnLessons.js
@@ -1,3 +1,8 @@
+import {hindiChapter1Lesson} from './hindiChapter1Engine';
+import {hindiChapter2Lesson} from './hindiChapter2Engine';
+import {hindiChapter3Lesson} from './hindiChapter3Engine';
+import {hindiChapter4Lesson} from './hindiChapter4Engine';
+import {hindiChapter5Lesson} from './hindiChapter5Engine';
 import {hindiChapter6Lesson} from './hindiChapter6Engine';
 import {hindiChapter7Lesson} from './hindiChapter7Engine';
 import {hindiChapter8Lesson} from './hindiChapter8Engine';
@@ -15,6 +20,7 @@ import {hindiPoetry7Lesson} from './hindiPoetry7Engine';
 import {hindiPoetry8Lesson} from './hindiPoetry8Engine';
 
 export const hindiLearnLessons={
+  g1:hindiChapter1Lesson,g2:hindiChapter2Lesson,g3:hindiChapter3Lesson,g4:hindiChapter4Lesson,g5:hindiChapter5Lesson,
   g6:hindiChapter6Lesson,g7:hindiChapter7Lesson,g8:hindiChapter8Lesson,g9:hindiChapter9Lesson,g10:hindiChapter10Lesson,g11:hindiChapter11Lesson,g12:hindiChapter12Lesson,
   p1:hindiPoetry1Lesson,p2:hindiPoetry2Lesson,p3:hindiPoetry3Lesson,p4:hindiPoetry4Lesson,p5:hindiPoetry5Lesson,p6:hindiPoetry6Lesson,p7:hindiPoetry7Lesson,p8:hindiPoetry8Lesson
 };
