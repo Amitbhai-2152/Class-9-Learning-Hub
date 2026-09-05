a6cad2587c7a6cb994918572960ffb10e7099f7e
Amit Raj <navodaya2152@gmail.com>
2026-09-05 13:34:56 +0530

    Load reload route restoration before app mount

--- src/main2.jsx
+++ src/main2.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import { AppErrorBoundary } from './AppErrorBoundary.jsx';
+import './reload-router.js';
 import './scienceModeRouter.js';
 import './subject-overrides.css';
 import './science-navigation.css';
