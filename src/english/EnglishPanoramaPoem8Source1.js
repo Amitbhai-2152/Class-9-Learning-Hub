76688829a086dad27efb7a6e4f633a2d8bc5cb84
Amit Raj <navodaya2152@gmail.com>
2026-09-08 23:50:58 +0530

    feat(english): add poem 8 source lines 1-10

---
+++ src/english/EnglishPanoramaPoem8Source1.js
@@ -0,0 +1,3 @@
+export const poem8Part1=[
+'He will have to learn, I know,','that all men are not just,','all men are not true.','But teach him also that','for every scoundrel there is a hero;','that for every selfish Politician,','there is a dedicated leader...','Teach him for every enemy there is','a friend.','It will take time, I know;'
+];
