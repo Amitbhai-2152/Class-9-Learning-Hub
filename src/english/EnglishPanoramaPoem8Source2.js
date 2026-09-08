f5185af80ab6777ba7bbfafaa7da32572a8195a6
Amit Raj <navodaya2152@gmail.com>
2026-09-08 23:51:05 +0530

    feat(english): add poem 8 source lines 11-20

---
+++ src/english/EnglishPanoramaPoem8Source2.js
@@ -0,0 +1,3 @@
+export const poem8Part2=[
+'but teach him if you can,','that a dollar earned is of far more value than five','pound...','Teach him to learn to lose...','and also to enjoy winning.','Steer him away from envy,','if you can,','teach him the secret of','quiet laughter.','Let him learn early that'
+];
