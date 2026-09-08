80eb9eff02975ecee05fdec278575ecd055085b7
Amit Raj <navodaya2152@gmail.com>
2026-09-08 23:51:44 +0530

    fix(english): complete poem 8 source lines 31-34

--- src/english/EnglishPanoramaPoem8Source4.js
+++ src/english/EnglishPanoramaPoem8Source4.js
@@ -1,2 +1,1 @@
-export const poem8Part4=[
-'Teach him to have faith','in his own ideas,','even if everyone tells him','they are wrong
+export const poem8Part4=['Teach him to have faith','in his own ideas,','even if everyone tells him','they are wrong...'];
