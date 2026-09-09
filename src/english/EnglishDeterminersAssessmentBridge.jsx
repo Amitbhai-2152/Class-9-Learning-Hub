48ad576482e2811c1e4e14184267895712517e20
Amit Raj <navodaya2152@gmail.com>
2026-09-09 21:19:18 +0530

    feat(english): bridge expanded determiners bank into shared quiz

---
+++ src/english/EnglishDeterminersAssessmentBridge.jsx
@@ -0,0 +1,4 @@
+import {PHASE4_BANKS} from './EnglishGenericLanguageSkillsBanks.jsx';
+import DETERMINERS_BANKS from './EnglishDeterminersQuestionBank.jsx';
+
+PHASE4_BANKS.determiners=DETERMINERS_BANKS;
