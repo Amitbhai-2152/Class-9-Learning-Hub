154f851cc7258bf9856c231decb17e0aa67487a5
Amit Raj <navodaya2152@gmail.com>
2026-09-13 15:07:02 +0530

    feat: show JNVST Class 9 upcoming exam

--- src/cbtConfig.js
+++ src/cbtConfig.js
@@ -1,5 +1,5 @@
 export const cbtConfig = {
   url: 'https://class9-bseb-cbt.netlify.app/',
   title: 'Class 9 BSEB CBT',
-  description: 'समयबद्ध परीक्षा, स्वतः परिणाम और ईमेल submission वाला CBT।',
+  description: 'समयबद्ध परीक्षा, स्वतः परिणाम और ईमेल submission वाला CBT।\n\nUpcoming Exam: JNVST Class 9 (2027 admission) • Official exam date will be announced by NVS.',
 };
